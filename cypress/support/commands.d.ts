64b70234f3ed42969509e2bad70e1472963fef37
Tom S <eldzik@gazeta.pl>
2022-10-26 14:22:44 +0200

    unify indentation

--- cypress/support/commands.d.ts
+++ cypress/support/commands.d.ts
@@ -1,5 +1,5 @@
 declare namespace Cypress {
-    interface Chainable<> {
-        logIn(): Cypress.Chainable;
-    }
+	interface Chainable<> {
+		logIn(): Cypress.Chainable;
+	}
 }
--- cypress/support/constants/default-values-constants.ts
+++ cypress/support/constants/default-values-constants.ts
@@ -1,3 +1,3 @@
 export class DefaultValuesConstants {
-    static MIN_BEDROOMS_FILTER_DEFAULT_VALUE = 0;
+  static MIN_BEDROOMS_FILTER_DEFAULT_VALUE = 0;
 }
